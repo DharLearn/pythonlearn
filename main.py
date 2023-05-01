6a211b9d2b189bba9cd244c404946b67844bbbd7
DharLearn <132243694+DharLearn@users.noreply.github.com>
2023-04-30 23:38:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-# Store input numbers
-num1 = input('Enter first number: ')
-num2 = input('Enter second number: ')
+from flask import Flask
+app = Flask(__name__)
 
-# Add two numbers
-sum = float(num1) + float(num2)
-
-# Display the sum
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+@app.route("/")
+def hello():
+    return "Hello, World!"
